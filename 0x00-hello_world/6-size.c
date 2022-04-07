efbdcca02ebfdc737e78da0d8e05dc6e66392556
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-07 14:58:27 -0700

    printf size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 /**main is entry point return: always 0*/
 int	main(void)
 {
-printf(size of a char: %d byte(s)\n. sizeof(char));
-printf(size of an int: %d byte(s)\n. sizeof(int));
-printf(size of a long int: %d byte(s)\n. sizeof(long int));
-printf(size of a long long int: %d byte(s)\n. sizeof(long long int));
-printf(size of a float: %d byte(s)\n. sizeof(float));
+printf(size_t of a char: %d byte(s)\n. sizeof(char));
+printf(size_t of an int: %d byte(s)\n. sizeof(int));
+printf(size_t of a long int: %d byte(s)\n. sizeof(long int));
+printf(size_t of a long long int: %d byte(s)\n. sizeof(long long int));
+printf(size_t of a float: %d byte(s)\n. sizeof(float));
 return (0);
 }
