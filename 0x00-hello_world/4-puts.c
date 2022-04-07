5d31edeba101796c7df1bc537fd63fbd0917c61e
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-07 12:22:56 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#!/bin/bash
+#include <stdio.h>
+/* main function is entry point and return 0 after printing the function */
+int main ( )
+         { puts("\" programming is like building a multilingual puzzle ");
+		 return (0); }
