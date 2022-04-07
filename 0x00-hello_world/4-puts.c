ef73379561db6ca4b9625fcc9d3df15127a437e1
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-07 13:34:32 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /*main function is entry point. return 0 after printing the function */
-int	main(void)  
+int	main(void) 
 {
 puts("\"programming is like building a multilingual puzzle");
 return	(0);
