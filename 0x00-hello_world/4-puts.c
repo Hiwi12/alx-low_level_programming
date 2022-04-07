8267b55a862e3fd4a0f752608ac0ad9d1c9a610c
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-07 12:56:54 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-/**main function is entry point
- * return 0 after printing the function */
-int main(void)
-         { puts("\"programming is like building a multilingual puzzle");
-		 return (0); }
+#include<stdio.h>
+/*main function is entry point. return 0 after printing the function */
+int	main(void)
+        { puts("\"programming is like building a multilingual puzzle");
+		 return	(0); }
