8214baa812750370c88c0275b38f4aa2668d35bb
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-08 03:06:36 -0700

    print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
  * main - Prints var size of var type
  * return: Always 0
  */
-int main(void)
+int main(void) {}
 {
-printf("size_t of a char: %d byte(s)\n", sizeof(char));
-printf("size_t of an int: %d byte(s)\n", sizeof(int));
-printf("size_t of a long int: %d byte(s)\n", sizeof(long int));
-printf("size_t of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("size_t of a float: %d byte(s)\n", sizeof(float));
+printf("Size_t of a char: %d byte(s)\n", sizeof(char));
+printf("Size_t of an int: %d byte(s)\n", sizeof(int));
+printf("Size_t of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size_t of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size_t of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
