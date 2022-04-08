89d788e0b983f5ab6f39db235bbd6d6b26968d74
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-08 00:39:34 -0700

    script for puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*main function is entry point. return 0 after printing the function */
 int	main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return	(0);
 }
