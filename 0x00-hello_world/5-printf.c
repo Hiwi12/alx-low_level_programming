79dc6e0b29479e384e996fa1ed42a4711c018ef5
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-08 14:47:34 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/**main = Entry point My hello, printf task and return one*/
+/**
+ * main - Entry point My hello, printf task and
+ *
+ * Return: one
+ */
 int	main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
