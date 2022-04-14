12d83ce99a29e596a72c3bc52a60a6e7583c5975
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-14 08:37:33 -0700

    mandatory tasks done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two intiger and return the result
+ *
+ * Return: intiger result
+ */
+int add(int, int)
+{
+	int a, b;
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for the alphabet
+ *
+ * Return: return 1 if it is alphabet otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
