fa301131a1e42d14c7baaccc52499f64abab1f62
Hiwi12 <hiwotgkidan3@gmail.com>
2022-04-16 01:57:22 -0700

    task mandatory

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two intiger
+ * @x: first intiger
+ * @y: second intiger
+ *
+ * Return: product of x and y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more tasks on function and nested loops
